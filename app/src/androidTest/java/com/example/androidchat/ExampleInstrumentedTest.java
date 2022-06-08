69ab138ba9a1e3574bf9fa7ee2c3e049e068bcf2
noaLeshem2 <noaleshem99@gmail.com>
2022-06-08 14:53:49 +0300

    first

--- app/src/androidTest/java/com/example/androidchat/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/androidchat/ExampleInstrumentedTest.java
@@ -25,6 +25,5 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.androidchat", appContext.getPackageName());
-        //jhgjhjjgjhgj
     }
 }
