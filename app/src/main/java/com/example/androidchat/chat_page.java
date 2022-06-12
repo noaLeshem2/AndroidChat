80052c88fc6f115c3cad95a65e5e3ef0edee608c
noaLeshem2 <noaleshem99@gmail.com>
2022-06-12 14:06:10 +0300

    hghg

--- app/src/main/java/com/example/androidchat/chat_page.java
+++ app/src/main/java/com/example/androidchat/chat_page.java
@@ -1,14 +1,30 @@
 package com.example.androidchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class chat_page extends AppCompatActivity {
 
+    private AppDB db;
+    private PostDao postDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
+
+        db = Room.databaseBuilder(getApplicationContext(), AppDB.class, "PostsDB").allowMainThreadQueries().build();
+        postDao = db.postDao();
+
+        Button addFriendBtn = findViewById(R.id.add_friend_btn);
+        addFriendBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, add_chat.class);
+            startActivity(intent);
+        });
+
+
     }
 }
