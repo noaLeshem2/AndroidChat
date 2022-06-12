35d6e18bbee216d8acde0a0f27ec97f045cf48b8
Inbal Apt <inbalapt@gmail.com>
2022-06-12 14:00:23 +0300

    kug

---
+++ app/src/main/java/com/example/androidchat/AppDB.java
@@ -0,0 +1,13 @@
+package com.example.androidchat;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {Post.class}, version = 1)
+public abstract class AppDB extends RoomDatabase{
+
+    public abstract PostDao postDao();
+
+}
---
+++ app/src/main/java/com/example/androidchat/Post.java
@@ -0,0 +1,38 @@
+package com.example.androidchat;
+
+import android.media.Image;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Post {
+
+    @PrimaryKey(autoGenerate=true)
+    private int id;
+    private String content;
+
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id1){
+        this.id = id1;
+    }
+
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String con){
+        this.content = con;
+    }
+
+    public Post(int id, String content, String time, Image image){
+        this.id=id;
+        this.content=content;
+    }
+
+    public Post(){
+    }
+}
