f5d12972ba10ae0ff022b916290a8e2e1fb9e021
noaLeshem2 <noaleshem99@gmail.com>
2022-06-14 11:02:04 +0300

    hghg

--- app/src/main/java/com/example/androidchat/api/WebServiceAPI.java
+++ app/src/main/java/com/example/androidchat/api/WebServiceAPI.java
@@ -4,9 +4,16 @@ import com.example.androidchat.User;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 public interface WebServiceAPI {
     @GET("users")
-    call<List<User>> getUsers();
+    Call<List<User>> getUsers();
 
     @POST("users")
     Call<void> createUser(@Body User user);
